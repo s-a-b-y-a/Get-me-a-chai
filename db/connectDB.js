27a9db4e1a313daea603bed458b07e0d1e79d6c9
Sabyasachi Biswal <sabyas2004@gmail.com>
2024-08-31 10:29:57 +0530

    getmeachai

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!global._mongoClientPromise) {
-  global._mongoClientPromise = mongoose.connect(MONGO_URI);
+  global._mongoClientPromise = mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 const connectDB = async () => {
